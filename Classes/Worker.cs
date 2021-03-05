127a4e2c9a45358ad6dffa8114a313ff244839a8
Vanquish875 <Vanquish87523@gmail.com>
2021-03-05 12:23:39 -0600

    Update Worker.cs

--- Classes/Worker.cs
+++ Classes/Worker.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using DotLToExcel.DotL;
 using DotLToExcel.Excel;
 using DotLToExcel.POCOS;
