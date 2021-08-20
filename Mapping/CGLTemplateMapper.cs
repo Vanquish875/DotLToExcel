74be34b1003fa66c057abef400872d4d3cdf23e5
Vanquish875 <Vanquish87523@gmail.com>
2021-08-20 10:21:03 -0500

    Add CGL Templates

---
+++ Mapping/CGLTemplateMapper.cs
@@ -0,0 +1,37 @@
+﻿using DotLToExcel.DotL;
+using DotLToExcel.POCOS;
+using System;
+using System.Collections.Generic;
+
+namespace DotLToExcel.Mapping
+{
+    public class CGLTemplateMapper
+    {
+        public List<TemplateDef> MapTemplateDef(IList<string> data)
+        {
+            int FieldLength = CGLTemplateFields.Fields.Length;
+            var templates = new List<TemplateDef>();
+
+            try
+            {
+                for(int i = 0; i < data.Count; i += FieldLength)
+                {
+                    var template = new TemplateDef();
+                    if(Helper.CheckIfCorrectTemplateName(data[i + 1]))
+                    {
+                        template.TemplateName = data[i];
+                        template.FieldName = data[i + 1];
+                        template.FieldValue = data[i + 2];
+                        template.FieldDesc = data[i + 3];
+                        templates.Add(template);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return templates;
+        }
+    }
+}
